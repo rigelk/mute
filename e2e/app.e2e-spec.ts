ac9a956d14c05caa8d98726d6e310d48833e73ba
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2018-01-15 14:17:16 +0100

    test(doc): Add test to check if document is correctly stored

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,10 @@
-import { browser } from 'protractor'
+import { browser, promise } from 'protractor'
+
+import { focusEditor, getEditorValue } from './doc.po'
+
+function sleep (duration: number): promise.Promise<void> {
+  return browser.driver.sleep(duration)
+}
 
 describe('mute App', () => {
 
@@ -8,5 +14,16 @@ describe('mute App', () => {
     await browser.get('/')
     const actualTitle = await browser.getTitle()
     expect(actualTitle).toEqual('Mute')
+  })
+
+  it('should store updates of the document', async () => {
+    const expectedText = 'Hello world !'
+    await browser.get('/test-e2e-recover')
+    await focusEditor(browser)
+    await browser.actions().sendKeys(expectedText).perform()
+    await sleep(1000) // Leave some time to the app to store updates
+    await browser.refresh()
+    const actualText = await getEditorValue(browser)
+    expect(actualText).toEqual(expectedText)
   })
 })
