174d251ca7377fd62ac930f334ec9be0654bb1e8
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2018-01-15 14:19:41 +0100

    test(doc): Add test to check if updates are correctly broadcasted

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { browser, promise } from 'protractor'
+import { browser, promise, protractor, ProtractorBrowser } from 'protractor'
 
 import { focusEditor, getEditorValue } from './doc.po'
 
@@ -25,5 +25,27 @@ describe('mute App', () => {
     await browser.refresh()
     const actualText = await getEditorValue(browser)
     expect(actualText).toEqual(expectedText)
+  })
+
+  it('should broadcast updates to peers', async () => {
+    const expectedText1 = 'Hellow'
+    const expectedText2 = 'Hello world !'
+    await browser.get('/test-e2e-broadcast')
+    const peerBrowser: ProtractorBrowser =  await browser.forkNewDriverInstance(true).ready
+    await sleep(1000) // Leave some time to the peers to connect
+
+    await focusEditor(browser)
+    await focusEditor(peerBrowser)
+
+    await browser.actions().sendKeys(expectedText1).perform()
+    await sleep(1000) // Leave some time to the app to broadcast updates
+    const actualText1 = await getEditorValue(peerBrowser)
+    expect(actualText1).toEqual(expectedText1)
+
+    const sequence = `${protractor.Key.BACK_SPACE} world !`
+    await peerBrowser.actions().sendKeys(sequence).perform()
+    await sleep(1000)
+    const actualText2 = await getEditorValue(browser)
+    expect(actualText2).toEqual(expectedText2)
   })
 })
